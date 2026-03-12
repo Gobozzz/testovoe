9562c75eca2c6157b9ddee5b492c685ef48eef9d
Gobozzz <gobozovbogdan@gmail.com>
2026-03-12 10:37:59 +0000

    add throttle for routes

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,10 @@ use App\Http\Controllers\Api\ProfileController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/ping', fn () => 'pong');
+Route::get('/ping', fn() => 'pong');
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:6,1');
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
@@ -16,7 +16,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('profile')->group(function () {
         Route::get('/', [ProfileController::class, 'get']);
         Route::put('/', [ProfileController::class, 'update']);
-        Route::post('/avatar', [ProfileController::class, 'updateAvatar']);
+        Route::post('/avatar', [ProfileController::class, 'updateAvatar'])->middleware('throttle:3,1');
         Route::delete('/avatar', [ProfileController::class, 'deleteAvatar']);
     });
 
